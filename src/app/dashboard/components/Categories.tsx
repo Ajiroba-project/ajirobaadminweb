0e8ce20719c7688715d3b2cc89a9d4f078a223ec
petsamuel <petsamuel4@gmail.com>
2024-05-02 14:08:19 +0100

    category table

--- src/app/dashboard/components/Categories.tsx
+++ src/app/dashboard/components/Categories.tsx
@@ -16,14 +16,39 @@ export const Categories = () => {
           : "mt-[30%] md:mt-[25%] mx-[5%] lg:mt-[10%] justify-center"
       }`}
     >
-      
-
       <div className="flex justify-between">
         <p> Category List</p>
-        <button className="bg-[#FCDFD4] p-2 px-5 rounded" onClick={handleClick}>Create</button>
+        <button className="bg-[#FCDFD4] p-2 px-5 rounded" onClick={handleClick}>
+          Create
+        </button>
       </div>
 
+      {/* table */}
 
+      <div className="container mx-auto w-full mt-5">
+        <table className="table-auto w-full text-center">
+          <thead className="bg-[#FCDFD4]">
+            <tr>
+              <th className="px-4 py-2 border-r-2 border-black ">S/N</th>
+              <th className="px-4 py-2">Category</th>
+              <th className="px-4 py-2">Subcategory</th>
+            </tr>
+          </thead>
+          <tbody className="bg-gray-50">
+            <tr>
+              <td className="text-center align-middle justify-center px-4 py-2">
+                2
+              </td>
+              <td className="text-center align-middle justify-center px-4 py-2">
+                Category 2
+              </td>
+              <td className="text-center align-middle justify-center px-4 py-2">
+                Subcategory 2
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 }
