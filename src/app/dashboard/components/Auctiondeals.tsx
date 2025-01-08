6ee7206a271d13b48d1985499893f2d9d9bbac99
femakin <santosfemakin@yahoo.com>
2025-01-08 16:20:02 +0100

    trans added

---
+++ src/app/dashboard/components/Auctiondeals.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Auctiondeals() {
+  return (
+    <div>Auctiondeals</div>
+  )
+}
+
+export default Auctiondeals
--- src/app/dashboard/components/Transaction.tsx
+++ src/app/dashboard/components/Transaction.tsx
@@ -1,72 +1,85 @@
-import {Card} from "./Card"
-import {regularDetails, auctionDetails} from "@/app/data"
-import {UserSearch} from './UserSearch'
-import {useStore } from '@/store/nav-store';
+import { Card } from "./Card";
+import { regularDetails, auctionDetails } from "@/app/data";
+import { UserSearch } from "./UserSearch";
+import { useStore } from "@/store/nav-store";
 import { section } from "framer-motion/client";
 import { div } from "framer-motion/m";
 import { IconButton } from "@/app/component/Button";
 import { MdOutlineFileDownload } from "react-icons/md";
 import { useState } from "react";
+import Regulardeals from "./Regulardeals";
+import Recharge from "./Recharge";
+import Auctiondeals from "./Auctiondeals";
 
+export const Transaction = () => {
+  const isNavbarOpen = useStore((state) => state.isNavbarOpen);
+  const [active, setActive] = useState(0);
 
+  return (
+    <div>
+      <div className="bg-[#F6F6F6] border border-b-[#e9dddd] h-auto  sticky top-0 ">
+        <div className="flex justify-between py-6 px-12 flex-wrap gap-4">
+          <div>
+            <h1 className="xl:text-2xl 2xl:text-2xl md:text-2xl text-base font-bold  leading-tight tracking-tight font-Poppins">
+              Gross Transaction Volume
+            </h1>
+          </div>
 
-export const Transaction =()=>{
-    const isNavbarOpen = useStore(state=> state.isNavbarOpen)
-        const [active, setActive] =useState(0)
-
-    return (
-
-       <div>
-
-  <div className="bg-[#F6F6F6] border border-b-[#e9dddd] h-auto  sticky top-0 ">
-
-      <div className="flex justify-between py-6 px-12 flex-wrap gap-4" >
-        <div>
-          <h1 className="xl:text-2xl 2xl:text-2xl md:text-2xl text-base font-bold  leading-tight tracking-tight font-Poppins">Gross Transaction Volume</h1>
+          <div>
+            <IconButton
+              type="button"
+              text="export Csv"
+              className="flex items-center gap-2 rounded-lg bg-[#F25E26] p-1 capitalize text-white w-fit justify-items-center"
+              icon={
+                <MdOutlineFileDownload className="text-base font-Poppins" />
+              }
+            />
+          </div>
         </div>
 
-        <div>
-             <IconButton
-          type='button'
-          text='export Csv'
-          className='flex items-center gap-2 rounded-lg bg-[#F25E26] p-1 capitalize text-white w-fit justify-items-center'
-          icon={<MdOutlineFileDownload className='text-base font-Poppins' />}
-        />
+        <div className="flex gap-4 2xl:gap-14 xl:gap-14 lg:gap-14 md:gap-14 flex-wrap py-6 px-12">
+          <div
+            className={`${
+              active == 0 ? "text-[#F25E26] underline underline-offset-8" : ""
+            }    cursor-pointer  `}
+            onClick={() => setActive(0)}
+          >
+            <h1>Regular Deals</h1>
+          </div>
+
+          <div
+            className={`${
+              active == 1 ? "text-[#F25E26] underline underline-offset-8" : ""
+            }    cursor-pointer  `}
+            onClick={() => setActive(1)}
+          >
+            <h1>Auction Deals</h1>
+          </div>
+
+          <div
+            className={`${
+              active == 2 ? "text-[#F25E26] underline underline-offset-8" : ""
+            }    cursor-pointer  `}
+            onClick={() => setActive(2)}
+          >
+            <h1>Recharge</h1>
+          </div>
         </div>
       </div>
 
 
 
-<div className="flex gap-4 2xl:gap-14 xl:gap-14 lg:gap-14 md:gap-14 flex-wrap py-6 px-12" >
-
-<div>
-  <h1>Regular Deals</h1>
-</div>
-
-<div>
-  <h1>Auction Deals</h1>
-</div>
-
-
-<div>
-  <h1>Recharge</h1>
-</div>
-
-</div>
-  </div>
-
-    <section className="mt-6 px-4">
-      <div className="flex gap-4 lg:flex-row flex-col lg:justify-center lg:items-center">
-        <Card title="Regular" object={regularDetails} />
-        <Card title="Auction" object={auctionDetails} />
+      <div className="py-6 px-12">
+ {active == 0 ? (
+        <Regulardeals />
+      ) : active == 1 ? (
+        <Auctiondeals />
+      ) : (
+        <Recharge />
+      )}
       </div>
 
-      <div className="mt-6 px-2">
-        <UserSearch />
-      </div>
-    </section>
-</div>
 
-
-    )
-}
+    </div>
+  );
+};
